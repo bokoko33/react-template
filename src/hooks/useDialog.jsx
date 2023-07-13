443d1e7f1d60687941cc388feaf9ac7a5ac3917b
bokoko33 <work.bokoko33@gmail.com>
2023-07-13 21:15:17 +0900

    update dialog

--- src/hooks/useDialog.jsx
+++ src/hooks/useDialog.jsx
@@ -1,28 +1,48 @@
 import styles from '~/styles/Dialog.module.scss';
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 
 export const useDialog = () => {
   const ref = useRef(null);
 
-  const toggleOpen = (newOpen) => {
+  const toggleOpen = useCallback((newOpen) => {
+    const dialogEl = ref.current;
     if (newOpen) {
-      ref.current.showModal();
+      // show()だとフォーカス制御やescで閉じられない
+      dialogEl.showModal();
+
+      // state更新だと再描画でdialogの開閉が合わなくなる
+      dialogEl.dataset.state = 'opening';
     } else {
-      ref.current.close();
+      dialogEl.dataset.state = 'closing';
+      dialogEl.onanimationend = () => {
+        dialogEl.close();
+        dialogEl.onanimationend = null;
+      };
     }
-  };
+  }, []);
 
   const Component = ({ children }) => (
     <dialog
       ref={ref}
-      className={styles.dialogSimple}
-      onClick={() => toggleOpen(false)}
+      className={styles.root}
+      onClick={() => {
+        // backdropクリックで閉じたい
+        toggleOpen(false);
+      }}
     >
       <div
-        className={styles.dialogContent}
-        onClick={(e) => e.stopPropagation()}
+        className={styles.content}
+        onClick={(e) => {
+          // backdropのみのクリックは判定できないので、
+          // content側の伝番を防ぐことで中身は触れるようにする
+          e.stopPropagation();
+        }}
       >
-        {children}
+        <button
+          className={styles.close}
+          onClick={() => toggleOpen(false)}
+        ></button>
+        <div className={styles.inner}>{children}</div>
       </div>
     </dialog>
   );
